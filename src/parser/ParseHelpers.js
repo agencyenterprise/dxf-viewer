c16c3f220e6428a9ab031f2b1feb647054116606
Daniel Cruz <danielcunha54@gmail.com>
2024-11-20 15:57:17 -0300

    Merge branch 'master' of https://github.com/agencyenterprise/dxf-viewer into master

--- src/parser/ParseHelpers.js
+++ src/parser/ParseHelpers.js
@@ -6,7 +6,7 @@ import ExtendedDataParser from './ExtendedDataParser';
  * @return {Number} truecolor value as a number
  */
 export function getAcadColor(index) {
-    return AUTO_CAD_COLOR_INDEX[index];
+  return AUTO_CAD_COLOR_INDEX[index];
 }
 
 /**
@@ -15,33 +15,31 @@ export function getAcadColor(index) {
  * @param {*} scanner
  */
 export function parsePoint(scanner) {
-    var point = {};
+  var point = {};
 
-    // Reread group for the first coordinate
-    scanner.rewind();
-    var curr = scanner.next();
+  // Reread group for the first coordinate
+  scanner.rewind();
+  var curr = scanner.next();
 
-    var code = curr.code;
-    point.x = curr.value;
+  var code = curr.code;
+  point.x = curr.value;
 
-    code += 10;
-    curr = scanner.next();
-    if(curr.code !== code)
-        throw new Error('Expected code for point value to be ' + code +
-        ' but got ' + curr.code + '.');
-    point.y = curr.value;
+  code += 10;
+  curr = scanner.next();
+  if (curr.code !== code)
+    throw new Error('Expected code for point value to be ' + code + ' but got ' + curr.code + '.');
+  point.y = curr.value;
 
-    code += 10;
-    curr = scanner.next();
-    if(curr.code !== code)
-    {
-        // Only the x and y are specified. Don't read z.
-        scanner.rewind(); // Let the calling code advance off the point
-        return point;
-    }
-    point.z = curr.value;
-
+  code += 10;
+  curr = scanner.next();
+  if (curr.code !== code) {
+    // Only the x and y are specified. Don't read z.
+    scanner.rewind(); // Let the calling code advance off the point
     return point;
+  }
+  point.z = curr.value;
+
+  return point;
 }
 
 /** Some entities may contain embedded object which is started by group 101. All the rest data until
@@ -50,16 +48,16 @@ export function parsePoint(scanner) {
  * @param scanner
  */
 export function skipEmbeddedObject(scanner) {
-    /* Ensure proper start group. */
-    scanner.rewind()
-    let curr = scanner.next()
-    if (curr.code !== 101) {
-        throw new Error("Bad call for skipEmbeddedObject()")
-    }
-    do {
-        curr = scanner.next()
-    } while (curr.code !== 0)
-    scanner.rewind()
+  /* Ensure proper start group. */
+  scanner.rewind();
+  let curr = scanner.next();
+  if (curr.code !== 101) {
+    throw new Error('Bad call for skipEmbeddedObject()');
+  }
+  do {
+    curr = scanner.next();
+  } while (curr.code !== 0);
+  scanner.rewind();
 }
 
 /**
@@ -69,72 +67,72 @@ export function skipEmbeddedObject(scanner) {
  * @param {*} curr - the current group being parsed
  */
 export function checkCommonEntityProperties(entity, curr, scanner) {
-    let xdataParser = null
-    while (curr.code >= 1000) {
-        if (xdataParser == null) {
-            xdataParser = new ExtendedDataParser()
-        }
-        if (xdataParser.Feed(curr)) {
-            xdataParser.Finish(entity)
-            xdataParser = null
-        } else {
-            curr = scanner.next()
-        }
+  let xdataParser = null;
+  while (curr.code >= 1000) {
+    if (xdataParser == null) {
+      xdataParser = new ExtendedDataParser();
     }
-    if (xdataParser) {
-        xdataParser.Finish(entity)
-        /* Groups following XDATA should be parsed on next iteration. */
-        scanner.rewind()
-        return true
+    if (xdataParser.Feed(curr)) {
+      xdataParser.Finish(entity);
+      xdataParser = null;
+    } else {
+      curr = scanner.next();
     }
-
-    switch(curr.code) {
-        case 0:
-            entity.type = curr.value;
-            break;
-        case 5:
-            entity.handle = curr.value;
-            break;
-        case 6:
-            entity.lineType = curr.value;
-            break;
-        case 8: // Layer name
-            entity.layer = curr.value;
-            break;
-        case 48:
-            entity.lineTypeScale = curr.value;
-            break;
-        case 60:
-            entity.visible = curr.value === 0;
-            break;
-        case 62: // Acad Index Color. 0 inherits ByBlock. 256 inherits ByLayer. Default is bylayer
-            entity.colorIndex = curr.value;
-            entity.color = getAcadColor(Math.abs(curr.value));
-            break;
-        case 67:
-            entity.inPaperSpace = curr.value !== 0;
-            break;
-        case 100:
-            //ignore
-            break;
-        case 330:
-            entity.ownerHandle = curr.value;
-            break;
-        case 347:
-            entity.materialObjectHandle = curr.value;
-            break;
-        case 370:
-            //From https://www.woutware.com/Forum/Topic/955/lineweight?returnUrl=%2FForum%2FUserPosts%3FuserId%3D478262319
-            // An integer representing 100th of mm, must be one of the following values:
-            // 0, 5, 9, 13, 15, 18, 20, 25, 30, 35, 40, 50, 53, 60, 70, 80, 90, 100, 106, 120, 140, 158, 200, 211.
-            // -3 = STANDARD, -2 = BYLAYER, -1 = BYBLOCK
-            entity.lineweight = curr.value;
-            break;
-        case 420: // TrueColor Color
-            entity.color = curr.value;
-            break;
-        default:
-            return false;
-    }
+  }
+  if (xdataParser) {
+    xdataParser.Finish(entity);
+    /* Groups following XDATA should be parsed on next iteration. */
+    scanner.rewind();
     return true;
+  }
+
+  switch (curr.code) {
+    case 0:
+      entity.type = curr.value;
+      break;
+    case 5:
+      entity.handle = curr.value;
+      break;
+    case 6:
+      entity.lineType = curr.value;
+      break;
+    case 8: // Layer name
+      entity.layer = curr.value;
+      break;
+    case 48:
+      entity.lineTypeScale = curr.value;
+      break;
+    case 60:
+      entity.hidden = !!curr.value;
+      break;
+    case 62: // Acad Index Color. 0 inherits ByBlock. 256 inherits ByLayer. Default is bylayer
+      entity.colorIndex = curr.value;
+      entity.color = getAcadColor(Math.abs(curr.value));
+      break;
+    case 67:
+      entity.inPaperSpace = curr.value !== 0;
+      break;
+    case 100:
+      //ignore
+      break;
+    case 330:
+      entity.ownerHandle = curr.value;
+      break;
+    case 347:
+      entity.materialObjectHandle = curr.value;
+      break;
+    case 370:
+      //From https://www.woutware.com/Forum/Topic/955/lineweight?returnUrl=%2FForum%2FUserPosts%3FuserId%3D478262319
+      // An integer representing 100th of mm, must be one of the following values:
+      // 0, 5, 9, 13, 15, 18, 20, 25, 30, 35, 40, 50, 53, 60, 70, 80, 90, 100, 106, 120, 140, 158, 200, 211.
+      // -3 = STANDARD, -2 = BYLAYER, -1 = BYBLOCK
+      entity.lineweight = curr.value;
+      break;
+    case 420: // TrueColor Color
+      entity.color = curr.value;
+      break;
+    default:
+      return false;
+  }
+  return true;
 }
